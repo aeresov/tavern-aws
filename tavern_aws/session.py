4f0937509fe3f87be5e3ab77169fbbdacde6652e
Alexander Eresov <aeresov@gmail.com>
2025-06-18 11:07:56 +0000

    wip

--- tavern_aws/session.py
+++ tavern_aws/session.py
@@ -20,24 +20,28 @@ class AWSSession(requests.Session):
         }
         check_expected_keys(expected_blocks, kwargs)
 
-        try:
-            profile = kwargs["profile"]
-            session = boto3.Session(profile_name=profile)
-            credentials = session.get_credentials()
-            if credentials is None:
-                raise RuntimeError("No credentials found")
-            auth = AWS4Auth(
-                access_id=credentials.access_key,
-                secret_key=credentials.secret_key,
-                region=kwargs["region"],
-                service=kwargs["service"],
-                session_token=credentials.token,
-            )
-            self.auth = auth
-        except KeyError as e:
-            msg = "Need to specify aws profile"
-            logger.error(msg)
-            raise exceptions.MissingKeysError(msg) from e
+        profile = kwargs.get("profile")
+        if not profile:
+            raise exceptions.MissingKeysError("Need to specify aws profile")
+        service = kwargs.get("service")
+        if not service:
+            raise exceptions.MissingKeysError("Need to specify aws service")
+        region = kwargs.get("region")
+        if not region:
+            raise exceptions.MissingKeysError("Need to specify aws region")
+
+        session = boto3.Session(profile_name=profile)
+        credentials = session.get_credentials()
+        if credentials is None:
+            raise RuntimeError("No credentials found")
+        auth = AWS4Auth(
+            access_id=credentials.access_key,
+            secret_key=credentials.secret_key,
+            region=region,
+            service=service,
+            session_token=credentials.token,
+        )
+        self.auth = auth
 
     def __enter__(self):
         pass
